b5a915ee5fd06e3877ea5b37c15499a70efe2f11
Niko <niko@straub.network>
2020-02-01 12:03:14 +0100

    api request

--- geofox-api.js
+++ geofox-api.js
@@ -1,31 +1,62 @@
-const crypto = require('crypto-js');
-
+const crypto = require("crypto");
+const rpn = requitre("request-promise-native");
+const apiEndpoint = "https://geofox.hvv.de/gti/public/";
 
 function createSignature(messageBody, apiSecretKey) {
-    let hmac = crypto.HmacSHA1(JSON.stringify(messageBody), apiSecretKey);
-    return hmac.digest('base64');
+  return crypto
+    .createHmac("sha1", apiSecretKey)
+    .update(JSON.stringify(messageBody))
+    .digest("hex");
 }
 
-module.exports = function (RED) {
-    function GeofoxApiNode(config) {
-        RED.nodes.createNode(this, config);
-        this.geofoxSecret = config.secret;
-        this.departure = config.departure;
-        let node = this;
-
-        node.on('input', function (msg) {
-            this.log("on input");
-            console.log("secret: " + node.geofoxSecret);
-            let msgBody = {
-                departure: node.departure
-            }
-
-            let signature = createSignature(msgBody, node.geofoxSecret);
-            this.log(`Signature: ${signature}`);
-
-            msg.payload = msg.payload;
-            node.send(msg);
-        });
+function departureList(station, signature) {
+  var options = {
+    uri: apiEndpoint + "departureList",
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "Request-Promise",
+      "X-Platform": "web",
+      "geofox-auth-user": "NikoStraub",
+      "geofox-auth-signature": signature
+    },
+    json: true,
+    method: "POST",
+    body: {
+      station: station,
+      time: "",
+      useRealtime: true
     }
-    RED.nodes.registerType("geofox-api", GeofoxApiNode);
+  };
+
+  rp(options)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
+
+module.exports = function(RED) {
+  async function GeofoxApiNode(config) {
+    RED.nodes.createNode(this, config);
+    this.geofoxSecret = config.secret;
+    this.departureStation = config.departure;
+    let node = this;
+
+    node.on("input", function(msg) {
+      this.log("on input");
+      let msgBody = {
+        departure: node.departure
+      };
+
+      const signature = createSignature(msgBody, node.geofoxSecret);
+      const departures = await departureList(departureStation, signature);
+
+
+      msg.payload = msg.payload;
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("geofox-api", GeofoxApiNode);
+};
