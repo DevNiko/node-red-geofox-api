06e51e1ec8b8d160d793419a20084e01d710f86d
Niko <niko@straub.network>
2020-02-02 11:09:27 +0100

    checkbname and departureList Endpoint

--- geofox-api.js
+++ geofox-api.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const rpn = require("request-promise-native");
+const moment = require("moment");
 const apiEndpoint = "https://geofox.hvv.de/gti/public/";
 
 function createSignature(messageBody, apiSecretKey) {
@@ -7,6 +8,59 @@ function createSignature(messageBody, apiSecretKey) {
     .createHmac("sha1", apiSecretKey)
     .update(JSON.stringify(messageBody))
     .digest("base64");
+}
+
+function buildRequestHeaders(apiUser, signature) {
+  return {
+    "Content-Type": "application/json;charset=UTF-8",
+    Accept: "application/json",
+    "User-Agent": "Request-Promise",
+    "X-Platform": "web",
+    "geofox-auth-type": "HmacSHA1",
+    "geofox-auth-user": apiUser,
+    "geofox-auth-signature": signature
+  };
+}
+
+function checkname(msgBody, apiUser, apiSecret) {
+  if (!apiSecret) {
+    return new Promise(function(resolve, reject) {
+      reject("No Geofox API Secret provided");
+    });
+  }
+
+  if (!apiUser) {
+    return new Promise(function(resolve, reject) {
+      reject("No Geofox User provided");
+    });
+  }
+
+  const signature = createSignature(msgBody, apiSecret);
+
+  var options = {
+    uri: apiEndpoint + "checkName",
+    headers: buildRequestHeaders(apiUser, signature),
+    json: true,
+    method: "POST",
+    body: msgBody
+  };
+
+  return rpn(options)
+    .then(function(response) {
+      return response;
+    })
+    .catch(function(err) {
+      let { statusCode, error } = err;
+      let { errorText, errDevInfo = "" } = error;
+      return new Error(
+        "Geofox API 'checkName' Request Error: " +
+          statusCode +
+          " - " +
+          errorText +
+          " - " +
+          errDevInfo
+      );
+    });
 }
 
 function departureList(msgBody, apiUser, apiSecret) {
@@ -26,14 +80,7 @@ function departureList(msgBody, apiUser, apiSecret) {
 
   var options = {
     uri: apiEndpoint + "departureList",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "User-Agent": "Request-Promise",
-      "X-Platform": "web",
-      "geofox-auth-user": apiUser,
-      "geofox-auth-signature": signature
-    },
+    headers: buildRequestHeaders(apiUser, signature),
     json: true,
     method: "POST",
     body: msgBody
@@ -46,7 +93,7 @@ function departureList(msgBody, apiUser, apiSecret) {
     .catch(function(err) {
       let { statusCode, error } = err;
       let { errorText, errDevInfo = "" } = error;
-      throw new Error(
+      return new Error(
         "Geofox API 'departureList' Request Error: " +
           statusCode +
           " - " +
@@ -66,36 +113,75 @@ module.exports = function(RED) {
     this.city = config.city;
     let node = this;
 
-    node.on("input", function(msg) {
+    node.on("input", async function(msg) {
       this.log("on input");
 
       let date = new Date();
       // Plus 60 Minutes from Now
       date.setMinutes(date.getMinutes() + 60);
+      moment.locale("de");
+
+      const cnRequestBody = {
+        coordinateType: "EPSG_4326",
+        maxList: 1,
+        theName: {
+          name: node.station,
+          type: "STATION"
+        }
+      };
+
+      const cnResponse = await checkname(
+        cnRequestBody,
+        node.apiUser,
+        node.apiSecret
+      );
+
+      let { results } = cnResponse;
+      let { id, city } = results[0];
+
+      // use corrected city name
+      if (city && node.city !== city) {
+        node.city = city;
+      }
 
       const msgBody = {
         station: {
-          name: this.station,
-          city: this.city,
-          combinedName: this.station + " " + this.city,
+          id: id,
+          name: node.station,
+          city: city,
+          combinedName: node.station + " " + node.city,
           type: "STATION"
         },
+        serviceTypes: ["ZUG"],
         time: {
-          date: date.toISOString().slice(0, 10),
-          time: date.getTime()
+          date: moment().format("L"),
+          time: moment().format("LT")
         },
         useRealtime: true
       };
 
-      msg.payload = departureList(msgBody, node.apiUser, node.apiSecret)
-        .then(function(response) {
-          return response;
-        })
-        .catch(function(err) {
-          console.log(err);
-          node.error(err);
-        });
+      const response = await departureList(
+        msgBody,
+        node.apiUser,
+        node.apiSecret
+      );
+      const { returnCode, errorText = "", error = {} } = response;
+      let payload = response;
 
+      if (returnCode === "ERROR_TEXT" && errorText !== "") {
+        payload = {
+          error: {
+            code: "Geofox API Error",
+            message: errorText
+          }
+        };
+      }
+
+      if (typeof error === Error) {
+        node.error(error);
+      }
+
+      msg.payload = payload;
       node.send(msg);
     });
   }
